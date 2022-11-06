5c2d883f7002b2168d111a8a90372f9dcbcd9651
Farhan Tanvir <farhanswe35@gmail.com>
2022-11-06 18:02:24 +0600

    upload project

---
+++ src/test/java/com/farhantanvir/demospringrabbitmqreceiver/DemoSpringRabbitmqReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farhantanvir.demospringrabbitmqreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringRabbitmqReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
